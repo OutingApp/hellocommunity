af90da9a3065a860ad4dd094b6ef692f1f66eaa1
Outing <34135617+OutingApp@users.noreply.github.com>
2018-01-08 16:56:50 +0100

    Update Crowdsale.sol

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,4 +1,4 @@
-@pragma solidity ^0.4.18;
+pragma solidity ^0.4.19;
 
 library SafeMath {
 
@@ -30,32 +30,33 @@ contract Crowdsale {
 
     using SafeMath for uint256;
 
-    address public beneficiary;
-    uint256 public constant hardCapInTokens = 20160000000000000; //
-    uint256 public fundingGoal;       								 //SoftCap
+    address public beneficiary = 0x8b7426A552AE68EbB8cb1C30295551B8D5A05304;
+    address addressOfTokenUsedAsReward = 0x77A4A5b3007EFa19B5D049B914a1271367E27FE4;
+    uint256 public constant hardCapInTokens = 20160000000000000; 
+    uint256 public fundingGoal = 800;       								 //SoftCap
     uint256 public amountRaised;
-    uint256 public deadline;
+    uint256 public deadline = now + 60720 minutes; 
     uint256 public price;
     token public tokenReward;
     uint256 public soldTokens;  								//Count Outing Tokens sold
     uint256 public restTokens = (hardCapInTokens - soldTokens);
     
-    uint256 public constant MIN_ETHER = 0.2 ether;     //Min amount of Ether 
-    uint256 public constant MAX_ETHER = 125 ether;              //Max amount of Ether
+    uint256 public constant MIN_ETHER = 0.1 ether;     //Min amount of Ether 
+    uint256 public constant MAX_ETHER = 90 ether;              //Max amount of Ether
 
     uint256 public START = now;                        //Start crowdsale
 
-    uint256 public TIER2 = now + 20160 minutes;        //Start + 14 days
+    uint256 public TIER2 = now + 20400 minutes;        //Start + 14 days
 
-    uint256 public TIER3 = now + 40320 minutes;        //Start + 28 days ( 14 days + 14 days)
+    uint256 public TIER3 = now + 40560 minutes;        //Start + 28 days ( 14 days + 14 days)
 
-    uint256 public TIER4 = now + 50400 minutes;        //Start + 35 days ( 14 days + 14 days + 7 days)
+    uint256 public TIER4 = now + 50640 minutes;        //Start + 35 days ( 14 days + 14 days + 7 days)
 
 
-    uint256 public constant TIER1_PRICE = 1032800;      //Price in 1st tier
-    uint256 public constant TIER2_PRICE = 1180300;      //Price in 2nd tier
-    uint256 public constant TIER3_PRICE = 1327800;      //Price in 3rd tier
-    uint256 public constant TIER4_PRICE = 1475400;      //Price in 4th tier
+    uint256 public constant TIER1_PRICE = 627000;      //Price in 1st tier
+    uint256 public constant TIER2_PRICE = 716600;      //Price in 2nd tier
+    uint256 public constant TIER3_PRICE = 806200;      //Price in 3rd tier
+    uint256 public constant TIER4_PRICE = 895700;      //Price in 4th tier
 
 
     mapping(address => uint256) public balanceOf;
@@ -65,12 +66,9 @@ contract Crowdsale {
     event GoalReached(address recipient, uint totalAmountRaised);
     event FundTransfer(address backer, uint amount, bool isContribution);
 
-    function Crowdsale (address ifSuccessfulSendTo, uint fundingGoalInEthers, uint durationInMinutes, address addressOfTokenUsedAsReward)
+    function Crowdsale ()
     public
     {
-        beneficiary = ifSuccessfulSendTo;
-        fundingGoal = fundingGoalInEthers * 1 ether;
-        deadline = now + durationInMinutes * 1 minutes;
         price = getPrice();
         tokenReward = token(addressOfTokenUsedAsReward);
     }
